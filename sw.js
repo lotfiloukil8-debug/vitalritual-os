b1d036fccdb44315e847aab45cd72ea1ecae0f1e
Lotfi <lotfi@example.com>
2026-09-03 02:56:54 +0200

    Initial commit - VitalRitual Web OS

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE_NAME = 'vitalritual-v4.0';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/favicon.svg',
+  '/manifest.json'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request).catch(() => caches.match('/index.html'));
+    })
+  );
+});
